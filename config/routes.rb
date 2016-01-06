5678b81901605bd9fb22388e7ef7496a66d2eaae
bernardoseven <bernardoseven@gmail.com>
2016-01-06 19:02:55 +0000

    added controllers, models, views, data, migration, seed, etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  
+  root 'cuisines#index'
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  resources :users
+  resources :cuisines, only: [:index, :show]
+  resources :recipes, only: [:show, :edit, :update, :destroy]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
